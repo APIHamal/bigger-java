6f64ed7c56869b62e0269b4932da5c93e8d48d8d
idealist <zhengpeng.li@outlook.com>
2025-04-15 01:00:41 +0800

    添加PageHelper插件

--- spring-cloud/mybatis-sample/src/main/java/com/lizhengpeng/bigger/java/DebugMain.java
+++ spring-cloud/mybatis-sample/src/main/java/com/lizhengpeng/bigger/java/DebugMain.java
@@ -1,19 +1,28 @@
 package com.lizhengpeng.bigger.java;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.InputStreamReader;
+import java.util.List;
 
 public class DebugMain {
     public static void main(String[] args) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder()
                 .build(new InputStreamReader(DebugMain.class.getClassLoader().getResourceAsStream("mybatis-config-datasource.xml")));
         SqlSession sqlSession = sqlSessionFactory.openSession();
         IUserDao userDao = sqlSession.getMapper(IUserDao.class);
-        User user = userDao.queryUserInfoById(1l);
-        System.out.println(user);
+        // 添加分页逻辑
+        PageHelper.startPage(1, 10);
+        List<User> users = userDao.fetchAll();
+        PageInfo<User> pageInfo = new PageInfo<>(users);
+        System.out.println(objectMapper.writeValueAsString(pageInfo));
         sqlSession.close();
+
     }
 }
--- spring-cloud/mybatis-sample/src/main/java/com/lizhengpeng/bigger/java/IUserDao.java
+++ spring-cloud/mybatis-sample/src/main/java/com/lizhengpeng/bigger/java/IUserDao.java
@@ -1,9 +1,11 @@
 package com.lizhengpeng.bigger.java;
 
+import java.util.List;
+
 /**
  * @Author liuguanyi
  * @Date 2025/1/26 上午10:57
  **/
 public interface IUserDao {
-	User queryUserInfoById(Long uId);
+	List<User> fetchAll();
 }
